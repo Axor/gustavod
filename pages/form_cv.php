b3f4ba1bf6c1cb9d16ea82f1c0c7fa882804c465
Gustavo Díaz <gadiaz3@gmail.com>
2016-07-03 12:33:14 -0500

    Mods 07/03
    Mods 07/03

---
+++ pages/form_cv.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>Gustavo Díaz - CV</title>
+			
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/main.css">
+		<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+<body>
+	<h1>Personal CV</h1>
+	<h2>Personal Information</h2>
+	<form action='' method='post'>
+		<p><label>Name</label><br /><input type='text' name='name' value=''></p> 
+		<p><label>Last Name</label><br /><input type='text' name='lname' value=''></p> 
+		<p><label>Email</label><br /><input type='email' name='email' value=''></p> 
+		<p><label>Address</label><br /><input type="text" name="address" value=""></p>
+		<p><label>Telephone</label><br /><input type='tel' name='telephone' value=''></p> 
+		<p><label>Cellphone</label><br /><input type='tel' name='cellphone' value=''></p>
+		<p><input type='submit' name='submit' value='Save'></p> 
+	</form>
+</body>
+
+</html>
